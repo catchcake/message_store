4dcd98df982ed64766ac7667d61f1a0c4fc0c437
Jindrich K. Smitka <jsmitka@smita.info>
2020-10-05 09:08:59 +0200

    Bump version: 0.4.2 → 1.0.0

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 ```elixir
 def deps do
   [
-    {:message_store, "~> 0.4.2"}
+    {:message_store, "~> 1.0.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule MessageStore.MixProject do
           :no_opaque
         ]
       ],
-      version: "0.4.2",
+      version: "1.0.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
