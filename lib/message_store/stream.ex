e134696670c5efd6698e1c526bd8c38897d24125
Jindrich K. Smitka <jsmitka@smita.info>
2020-12-11 16:30:55 +0100

    Added read function

--- lib/message_store/stream.ex
+++ lib/message_store/stream.ex
@@ -4,20 +4,25 @@ defmodule MessageStore.Stream do
   """
 
   def read(stream_name) do
-    nil
+    # XXX: for testing purposes
+    Postgrex.query(
+      Core.MessageStore.Postgrex,
+      query_read_stream("eventstore"),
+      [stream_name]
+    )
   end
 
-  def query_read_stream() do
+  def query_read_stream(schema) do
     """
     with event_ids as (
       select event_id
-      from stream_events join streams using(stream_id)
-      where streams.stream_uuid = $1
+      from #{schema}.stream_events join #{schema}.streams using(stream_id)
+      where #{schema}.streams.stream_uuid = $1
     )
     select *
     from event_ids
-      join stream_events using(event_id)
-      join events using(event_id)
+      join #{schema}.stream_events using(event_id)
+      join #{schema}.events using(event_id)
     where stream_id=0;
     """
   end
