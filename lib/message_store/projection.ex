0ffbb4e5eb4ab6386b4bb5d7bbb73977803755eb
Jindrich K. Smitka <jsmitka@smita.info>
2021-06-29 15:16:17 +0200

    Added projection helpers

--- lib/message_store/projection.ex
+++ lib/message_store/projection.ex
@@ -1,7 +1,9 @@
 defmodule MessageStore.Projection do
   @moduledoc """
-  A projection boilerplate.
+  A projection boilerplate and helpers.
   """
+
+  @type fetch() :: (String.t(), module() -> Result.t(term(), any()))
 
   defmacro __using__(_env) do
     quote do
@@ -18,5 +20,14 @@ defmodule MessageStore.Projection do
       # default handler when unknown event arrived
       def handle_message(_, data), do: data
     end
+  end
+
+  @spec load(String.t(), module(), fetch(), (any() -> any())) :: any()
+  def load(stream, projection, fetch, fun)
+      when is_binary(stream) and is_atom(projection) and is_function(fetch, 2) and
+             is_function(fun, 1) do
+    {:ok, data} = fetch.(stream, projection)
+
+    fun.(data)
   end
 end
