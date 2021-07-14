202cb0fafdd377ef1555efe72fa0795087c9d504
Jindrich K. Smitka <jsmitka@smita.info>
2021-07-14 15:20:50 +0200

    Renamed event to message

--- lib/message_store/message.ex
+++ lib/message_store/message.ex
@@ -1,6 +1,6 @@
 defmodule MessageStore.Message do
   @moduledoc """
-  A module for build, copy and folow event data.
+  A module for build, copy and folow message data.
   """
 
   alias EventStore.EventData
@@ -14,32 +14,32 @@ defmodule MessageStore.Message do
   defguard is_version(version) when is_atom(version) or (is_integer(version) and version >= 0)
   defguard is_module_name(module) when is_atom(module) and module not in [nil, true, false]
 
-  def build(event) when is_map(event) do
+  def build(message) when is_map(message) do
     %EventData{
-      event_id: Map.get(event, :event_id),
-      event_type: event |> Map.fetch!(:type) |> to_string(),
-      data: Map.fetch!(event, :data),
-      metadata: Map.fetch!(event, :metadata),
-      causation_id: Map.get(event, :causation_id, nil),
-      correlation_id: Map.get(event, :correlation_id, nil)
+      event_id: Map.get(message, :event_id),
+      event_type: message |> Map.fetch!(:type) |> to_string(),
+      data: Map.fetch!(message, :data),
+      metadata: Map.fetch!(message, :metadata),
+      causation_id: Map.get(message, :causation_id, nil),
+      correlation_id: Map.get(message, :correlation_id, nil)
     }
   end
 
-  def copy(event, recorded_event, copy_list) do
+  def copy(message, recorded_message, copy_list) do
     copy_list
-    |> Enum.map(&get_data_from(&1, recorded_event))
-    |> Enum.reduce(build(event), &update_event(&1, &2))
+    |> Enum.map(&get_data_from(&1, recorded_message))
+    |> Enum.reduce(build(message), &update_message(&1, &2))
   end
 
-  def follow(event, recorded_event, copy_list)
-      when is_map(event) and is_map(recorded_event) and is_list(copy_list) do
-    event
-    |> copy(recorded_event, copy_list)
+  def follow(message, recorded_message, copy_list)
+      when is_map(message) and is_map(recorded_message) and is_list(copy_list) do
+    message
+    |> copy(recorded_message, copy_list)
     |> Map.put(
       :correlation_id,
-      recorded_event.correlation_id |> Maybe.with_default(recorded_event.event_id)
+      recorded_message.correlation_id |> Maybe.with_default(recorded_message.event_id)
     )
-    |> Map.put(:causation_id, recorded_event.event_id)
+    |> Map.put(:causation_id, recorded_message.event_id)
   end
 
   @spec type_from_module(module()) :: String.t()
@@ -78,13 +78,13 @@ defmodule MessageStore.Message do
     {domd, Map.get(map, domd) |> Map.take(keys)}
   end
 
-  defp update_event({key, map}, event) when is_data_or_metadata(key) and is_map(map) do
-    updated_event_domd = Map.merge(event[key], map)
+  defp update_message({key, map}, message) when is_data_or_metadata(key) and is_map(map) do
+    updated_message_domd = Map.merge(message[key], map)
 
-    Map.put(event, key, updated_event_domd)
+    Map.put(message, key, updated_message_domd)
   end
 
-  defp update_event({key, data}, event) when is_atom(key) do
-    Map.put(event, key, data)
+  defp update_message({key, data}, message) when is_atom(key) do
+    Map.put(message, key, data)
   end
 end
