7c68cf1054557cd428a182496d70d130741109df
Jindrich K. Smitka <jsmitka@smita.info>
2022-05-11 10:25:55 +0200

    Added support code to dev environment

--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,12 @@
 use Mix.Config
+
+eventstore_url = "#{System.fetch_env!("EVENTSTORE_URL")}"
+
+config(:message_store, TestMessageStore,
+  url: eventstore_url,
+  serializer: MessageStore.JsonSerializer,
+  column_data_type: "jsonb",
+  types: EventStore.PostgresTypes
+)
+
+config(:message_store, event_stores: [TestMessageStore])
--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule MessageStore.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(env) when env in [:dev, :test], do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
